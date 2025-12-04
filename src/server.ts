bc4b636e254b5ecd0b2b10695478022128ffa887
Sifat2245 <mdsifat1015@gmail.com>
2025-12-04 09:13:46 +0600

    created the server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import http, { IncomingMessage, Server, ServerResponse } from 'http'
+import config from './config/index.ts';
+
+const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) =>{
+    console.log('server is initialized...');
+
+    if(req.url === '/' && req.method === 'GET'){
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(
+            JSON.stringify({
+                message: "hello from node js server which is build with typescript",
+                path: req.url,
+            })
+        )
+    };
+})
+
+server.listen(config.port, () =>{
+    console.log(`server is running in port ${config.port}`);
+})
