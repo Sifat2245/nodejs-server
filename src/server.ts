69a797c229a8c3ef601fbdee7b1436df2db5fb46
Sifat2245 <mdsifat1015@gmail.com>
2025-12-05 09:14:00 +0600

    added responsve

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,58 @@
-import http, { IncomingMessage, Server, ServerResponse } from 'http'
-import config from './config/index.ts';
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config/index.ts";
 
-const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) =>{
-    console.log('server is initialized...');
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("server is initialized...");
 
-    if(req.url === '/' && req.method === 'GET'){
-        res.writeHead(200, {'content-type': 'application/json'});
-        res.end(
-            JSON.stringify({
-                message: "hello from node js server which is build with typescript",
-                path: req.url,
-            })
-        )
-    };
-})
+    if (req.url === "/" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "hello from node js server which is build with typescript",
+          path: req.url,
+        })
+      );
+    }
 
-server.listen(config.port, () =>{
-    console.log(`server is running in port ${config.port}`);
-})
+    if (req.url === "/api" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "hello!!! status ok",
+          path: req.url,
+        })
+      );
+    }
+
+    if (req.url === "/api/user" && req.method === "POST") {
+      //   const user = {
+      //     id: 1,
+      //     name: "alice",
+      //   };
+      //   res.writeHead(200, { "content-type": "application/json" });
+      //   res.end(JSON.stringify(user));
+
+      let body = "";
+      // listen for data chunk
+
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+          console.log('hello');
+          res.end(JSON.stringify(parseBody));
+        } catch (err: any) {
+          console.log(err);
+        }
+      });
+    }
+  }
+);
+
+server.listen(config.port, () => {
+  console.log(`server is running in port ${config.port}`);
+});
