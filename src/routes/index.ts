17704646070804a27ef47ca79cbf38c65858bee8
Sifat2245 <mdsifat1015@gmail.com>
2025-12-10 09:37:08 +0600

    created fake json

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import { readUsers, writeUsers } from "../helper/fileDB.ts";
 import SendJson from "../helper/JsonSender.ts";
 import { parseBody } from "../helper/parseBody.ts";
 import addRoutes from "../helper/routeHandler.ts";
@@ -11,5 +12,19 @@ addRoutes("GET", "/", (req, res) => {
 
 addRoutes("POST", "/api/users", async(req, res) =>{
   const body = await parseBody(req);
+
+  //reading users json
+  const users = readUsers();
+
+  
+  const newUser = {
+    id: users.length + 1,
+    ...body
+  };
+
+  users.push(newUser)
+
+  writeUsers(users)
+
   SendJson(res, 201, {data: body, success: true})
 })
